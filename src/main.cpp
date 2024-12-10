e988219931d3ad22742a6f75c4faca4dd8e9d111
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2024-12-10 21:08:51 +0100

    Expand FW

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,13 @@
 
 void setup() {
     Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop () {
-
+    Serial.println("Blinky!");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
